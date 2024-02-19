0b5e47b3395149d31ca928025817226d8bae0ab2
Sang oh yeh <sangohyeh@gmail.com>
2024-02-19 19:41:20 +0900

    Update error pages and remove unused files

--- apps/yeh35_blog/lib/yeh35_blog_web/controllers/page_controller.ex
+++
@@ -1,9 +0,0 @@
-defmodule Yeh35BlogWeb.PageController do
-  use Yeh35BlogWeb, :controller
-
-  def home(conn, _params) do
-    # The home page is often custom made,
-    # so skip the default app layout.
-    render(conn, :home, layout: false)
-  end
-end
--- apps/yeh35_blog/lib/yeh35_blog_web/controllers/page_html.ex
+++
@@ -1,5 +0,0 @@
-defmodule Yeh35BlogWeb.PageHTML do
-  use Yeh35BlogWeb, :html
-
-  embed_templates "page_html/*"
-end
